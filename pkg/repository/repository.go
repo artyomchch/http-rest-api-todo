a0d60426e9c28398db04201c47dd6933567139b7
Artyom Kozlov <artyom.ch.ch@gmail.com>
2021-04-30 12:22:06 +0300

    10 lesson

--- pkg/repository/repository.go
+++ pkg/repository/repository.go
@@ -14,6 +14,8 @@ type TodoList interface {
 	Create(userId int, list http_rest_api_test.TodoList) (int, error)
 	GetAll(userId int) ([]http_rest_api_test.TodoList, error)
 	GetById(userId, listId int) (http_rest_api_test.TodoList, error)
+	Delete(userId, listId int) error
+	Update(userId, listId int, input http_rest_api_test.UpdateListInput) error
 }
 
 type TodoItem interface {
--- pkg/service/todo_list.go
+++ pkg/service/todo_list.go
@@ -24,3 +24,14 @@ func (s *TodoListService) GetAll(userId int) ([]http_rest_api_test.TodoList, err
 func (s *TodoListService) GetById(userId, listId int) (http_rest_api_test.TodoList, error) {
 	return s.repo.GetById(userId, listId)
 }
+
+func (s *TodoListService) Delete(userId, listId int) error {
+	return s.repo.Delete(userId, listId)
+}
+
+func (s *TodoListService) Update(userId, listId int, input http_rest_api_test.UpdateListInput) error {
+	if err := input.Validate(); err != nil {
+		return err
+	}
+	return s.repo.Update(userId, listId, input)
+}
